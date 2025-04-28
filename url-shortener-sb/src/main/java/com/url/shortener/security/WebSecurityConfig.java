8e0ca08c09d30b9338c37c6c4410958fa7ba527c
navin <kartikmaurya18@gmail.com>
2025-04-28 16:49:58 +0530

    first commit

---
+++ url-shortener-sb/src/main/java/com/url/shortener/security/WebSecurityConfig.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int [] ans=new int[2];
+        int n=nums.length;
+        HashMap <Integer, Integer> mpp=new HashMap<>();
+        for (int i=0;i<n;i++){
+            int num=nums[i];
+            int moreneed=target-num;
+            if (mpp.containsKey(moreneed)){
+                ans[0]=mpp.get(moreneed);
+                ans[1]=i;
+return ans;
+            }
+                            mpp.put(ans[0], ans[1]);
+
+        }
+        return ans;
+}
+}
